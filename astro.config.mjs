371cd536f9fe9610943a25f24eebc844399d5955
Sterly Deracy <SDeracy@gmail.com>
2026-01-11 14:03:57 -0500

    Improves SEO and addes google meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://www.sederacybooks.com',
-  integrations: [
-    react(),
-    tailwind({
-      configFile: './tailwind.config.mjs' 
-    })
-  ]
+  integrations: [react(), tailwind({
+    configFile: './tailwind.config.mjs' 
+  }), sitemap()]
 });
