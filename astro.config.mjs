a06acfa082dc2c9368940b256c93a37c72ad88af
Sterly Deracy <SDeracy@gmail.com>
2026-01-10 20:13:39 -0500

    Adds site references + deployment.yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://www.sederacybooks.com',
   integrations: [
     react(),
     tailwind({
